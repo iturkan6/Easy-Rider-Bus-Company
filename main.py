d8f48e06e5335e8980b27e7138cbcf30fde85dc0
Tina <iturkan6@gmail.com>
2022-04-21 21:37:36 +0400

    # code refactoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def take_info():
     errors = {"bus_id": 0, "stop_id": 0, "stop_name": 0, "next_stop": 0, "stop_type": 0, "a_time": 0}
     stop_types = ("S", "O", "F")
     for dictionary in data:
-        if type(dictionary["bus_id"]) is not int:
+        if dict_data(dictionary, "bus_id") is not int:
             errors["bus_id"] += 1
         if type(dictionary["stop_id"]) is not int:
             errors["stop_id"] += 1
@@ -17,17 +17,20 @@ def take_info():
             errors["stop_type"] += 1
         if type(dictionary["a_time"]) is not str or not dictionary["a_time"]:
             errors["a_time"] += 1
-    print(f"""Type and required field validation: {sum(errors.values())} errors
-bus_id: {errors["bus_id"]}
-stop_id: {errors["stop_id"]}
-stop_name: {errors["stop_name"]}
-next_stop: {errors["next_stop"]}
-stop_type: {errors["stop_type"]}
-a_time: {errors["a_time"]}""")
+    print("""Type and required field validation: {} errors
+bus_id: {}
+stop_id: {}
+stop_name: {}
+next_stop: {}
+stop_type: {}
+a_time: {}""".format(sum(errors.values()), *errors.values()))
+
+
+def dict_data(dictionary: dict, key: str):
+    return type(dictionary[key])
 
 
 if __name__ == "__main__":
     s = input()
     data = json.loads(s)
     take_info()
-
