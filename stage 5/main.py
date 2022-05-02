be0e8341e1ee1927dc69235e9b3a53334895542f
iturkan6 <iturkan6@gmail.com>
2022-05-03 02:43:36 +0400

    # solve 5th stage

--- stage 5/main.py
+++ stage 5/main.py
@@ -7,15 +7,17 @@ def take_info() -> None:
         cur_bus = data[i]
         next_bus = data[i + 1]
         bus_id = cur_bus["bus_id"]
-        cur_time = time.strptime(cur_bus["a_time"], "%H:%M")
-        next_time = time.strptime(next_bus["a_time"], "%H:%M")
-        if cur_time > next_time and bus_id == next_bus["bus_id"] and bus_id not in lines:
+        if conv_time(cur_bus) > conv_time(next_bus) and bus_id == next_bus["bus_id"] and bus_id not in lines:
             lines[bus_id] = next_bus["stop_name"]
     print("Arrival time test:")
     if lines:
         [print("bus_id line ", k, ": wrong time on station ", v, sep="") for k, v in lines.items()]
     else:
         print("OK")
+
+
+def conv_time(bus):
+    return time.strptime(bus["a_time"], "%H:%M")
 
 
 if __name__ == "__main__":
